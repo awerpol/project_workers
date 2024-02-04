4154785d0119f6f9289832c13bd7dd70c57b7dae
Maxim Sivakon <m.sivakon@yandex.com>
2024-02-04 22:21:36 +0700

    init new tamplte and del. old templ

---
+++ local/templates/lineone_work/description.php
@@ -0,0 +1,8 @@
+<?
+$arTemplate = array (
+  'NAME' => 'lineone_work',
+  'DESCRIPTION' => 'lineone_work',
+  'SORT' => '1',
+  'TYPE' => '',
+);
+?>
---
+++ local/templates/lineone_work/header.php
@@ -0,0 +1,31 @@
+<?
+/**
+ * @global $APPLICATION
+ * @global $USER
+ */
+
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+use Bitrix\Main\Application;
+use Bitrix\Main\Page\Asset;
+
+global $APPLICATION;
+
+?>
+<!DOCTYPE html>
+<html lang="<?= LANGUAGE_ID ?>">
+<head>
+    <title><? $APPLICATION->ShowTitle() ?></title>
+    <?
+    $APPLICATION->ShowHead();
+    $APPLICATION->AddHeadString('<meta charset="UTF-8">');
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH."/assets/css/bootstrap.min.css");
+    $APPLICATION->AddHeadString('<script type="text/javascript" src="'.SITE_TEMPLATE_PATH.'/moscow/js/jquery-3.5.1.min.js" data-skip-moving="true"></script>');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH."/moscow/js/slick.js");
+    ?>
+</head>
+<body>
+<? $APPLICATION->ShowPanel() ?>
+
